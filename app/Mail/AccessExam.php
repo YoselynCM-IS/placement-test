cd2819cdaecf8024663a7493b32441a5557496ff
Genaro Perez <genaromajestice@gmail.com>
2023-06-15 20:49:42 -0600

    quitar job de emails

--- app/Mail/AccessExam.php
+++ app/Mail/AccessExam.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class AccessExam extends Mailable implements ShouldQueue
+class AccessExam extends Mailable
 {
     use Queueable, SerializesModels;
 
